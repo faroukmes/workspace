be70e4898589c1bfe9aac09c9b09ceaf2d700ba5
faroukmes <messaoudifraouk@gmail.com>
2026-02-03 02:50:48 +0100

    Implement MongoDB operations in vehicle.ts
    Added MongoDB operations for contact management.

--- checkpoints/TypeScriptCheckPoint/vehicle.ts
+++ checkpoints/TypeScriptCheckPoint/vehicle.ts
@@ -1,30 +1,44 @@
-// 1. Define the Vehicle interface
-interface Vehicle {
-    make: string;
-    model: string;
-    year: number;
-    start(): void;
-}
+// 1. Create database and collection
+use contact
+db.createCollection("contactlist")
 
-// 2. Implement the Car class
-class Car implements Vehicle {
-    // Properties from the Vehicle interface
-    make: string;
-    model: string;
-    year: number;
+// 2. Insert documents
+db.contactlist.insertMany([
+    { lastName: "Ben", firstName: "Moris", email: "ben@gmail.com", age: 26 },
+    { lastName: "Kefi", firstName: "Seif", email: "kefi@gmail.com", age: 15 },
+    { lastName: "Emilie", firstName: "brouge", email: "emilie.b@gmail.com", age: 40 },
+    { lastName: "Alex", firstName: "brown", age: 4 },
+    { lastName: "Denzel", firstName: "Washington", age: 3 }
+])
 
-    // Constructor to initialize properties
-    constructor(make: string, model: string, year: number) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
-    }
+// 3. Display all contacts
+db.contactlist.find({})
 
-    start(): void {
-        console.log("Car engine started");
-    }
-}
+// 4. Find one person by ID (example using Kefi's ID)
+// Get the _id first
+let kefi = db.contactlist.findOne({ lastName: "Kefi", firstName: "Seif" })
+db.contactlist.find({ _id: kefi._id })
 
-const myCar = new Car("Toyota", "Camry", 2025);
+// 5. Contacts with age > 18
+db.contactlist.find({ age: { $gt: 18 } })
 
-myCar.start();
+// 6. Age > 18 and name containing "ah"
+db.contactlist.find({ 
+    age: { $gt: 18 },
+    $or: [
+        { firstName: /ah/i },
+        { lastName: /ah/i }
+    ]
+})
+
+// 7. Change Kefi Seif to Kefi Anis
+db.contactlist.updateOne(
+    { lastName: "Kefi", firstName: "Seif" },
+    { $set: { firstName: "Anis" } }
+)
+
+// 8. Delete contacts aged < 5
+db.contactlist.deleteMany({ age: { $lt: 5 } })
+
+// 9. Display all contacts again
+db.contactlist.find({})
